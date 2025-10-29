e11687c96817e5c5d173ae645f9a4859b36ed893
Matheus Marchesotti <mmarferraz@gmail.com>
2025-10-29 15:00:39 -0300

    Initial commit of core configuration

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,48 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- when on visual mode, J and K moves the selection (like alt + arrow)
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- when using J (append line below to current), keeps cursor at position
+-- instead of moving it to the end of line
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- half page jumping always centers
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- moving search terms always centers
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- replace without overwriting buffer
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- delete without overwriting buffer
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+-- copy to system clipboard (+y buffer)
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- new tmux session
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+-- format current buffer
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- quick fix navigation
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- use current word on replace menu
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
---
+++ .config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,1 @@
+/home/mmartti/.config/omarchy/current/theme/neovim.lua
